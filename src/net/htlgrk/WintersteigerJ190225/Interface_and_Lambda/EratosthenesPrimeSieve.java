4e751a05081404bda9bb56fb8c19319c2a7fff68
Jakob <wintersteigerJ190225@sus.htl-grieskirchen.at>
2022-09-22 10:06:40 +0200

    added Sieve of Eratosthenes Algorithm

--- src/net/htlgrk/WintersteigerJ190225/Interface_and_Lambda/EratosthenesPrimeSieve.java
+++ src/net/htlgrk/WintersteigerJ190225/Interface_and_Lambda/EratosthenesPrimeSieve.java
@@ -1,2 +1,30 @@
-package net.htlgrk.WintersteigerJ190225.Interface_and_Lambda;public class EratosthenesPrimeSieve {
+package net.htlgrk.WintersteigerJ190225.Interface_and_Lambda;
+
+import java.util.ArrayList;
+
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    @Override
+    public boolean isPrime(int p) {
+        ArrayList<Boolean> numbers = new ArrayList<>();
+        numbers.forEach(n -> n = true);
+
+        for(int n = 2; p*p <=p; p++)
+        {
+            // If prime[p] is not changed, then it is a prime
+            if(numbers.get(n) == true)
+            {
+                // Update all multiples of p
+                for(int i = n*n; i <= p; i += p)
+                    numbers.set(i, false);
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
 }
--- src/net/htlgrk/WintersteigerJ190225/Interface_and_Lambda/PrimeSieve.java
+++ src/net/htlgrk/WintersteigerJ190225/Interface_and_Lambda/PrimeSieve.java
@@ -1,2 +1,6 @@
-package net.htlgrk.WintersteigerJ190225.Interface_and_Lambda;public interface PrimeSieve {
+package net.htlgrk.WintersteigerJ190225.Interface_and_Lambda;
+
+public interface PrimeSieve {
+    public boolean isPrime(int p);
+    public void printPrimes();
 }
